b4134a007a8ad79f496a1998f05e71476770c8a2
ChadH28 <61545754+ChadH28@users.noreply.github.com>
2020-03-09 11:49:01 +0200

    Add files via upload

---
+++ Find X exercise.py
@@ -0,0 +1,11 @@
+A=4
+B=8
+C=4
+D=10
+print (" Equation: X = (A-3)**0.5/ (B**2 + C**2 + D**2) ")
+print("A=4")
+print("B=8")
+print("C=4")
+print("D=10")
+print("Find X:")
+print ("X =", (A-3)**0.5/ (B**2 + C**2 + D**2))
