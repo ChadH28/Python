33d90ba738445aae9a69ebd1d2840dd48e208a10
ChadH28 <61545754+ChadH28@users.noreply.github.com>
2020-03-09 11:21:30 +0200

    Update Exercise.py

--- Exercise.py
+++ Exercise.py
@@ -2,6 +2,6 @@ pi=3.14
 Rad_squared=5**2
 print("Pi = 3.14")
 print ("Radius Squared = 5**2")
-print("Find Area:")
+print("Find Area of the Circle:")
 print ("A = Radius Squared x pi")
 print ("A = ", pi * Rad_squared)
